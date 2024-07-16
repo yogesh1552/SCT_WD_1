25cc08987d81c8baa057b2bc03009bff4cd8d8bc
yogesh1552 <108211499+yogesh1552@users.noreply.github.com>
2024-07-16 20:20:49 +0530

    Add files via upload

---
+++ qwerty.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('nav.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
